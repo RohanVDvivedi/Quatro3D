c7b102e854c5635e47afed83bccb87e0d9f70cf1
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-06 17:59:38 +0530

    test case for angle between vectors implemented

--- test/test.c
+++ test/test.c
@@ -3,7 +3,19 @@
 
 #include<quatro.h>
 
+void print_vector(vector v)
+{
+	printf("(%f %f %f)", v.xi, v.yj, v.zk);
+}
+
 int main()
 {
+	vector v1 = {0,1,0};
+	vector v2 = {1,0,0};
+	print_vector(v1);
+	print_vector(v2);
+	printf("->%f\n", angle_between_vectors(&v1, &v2) * 180 / M_PI);
+
+
 	return 0;
 }
