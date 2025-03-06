eaa2fa33d6e86c9636c4878d33832007fa3c7b54
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-06 10:47:36 +0530

    struct definitions added

--- inc/quatro.h
+++ inc/quatro.h
@@ -7,4 +7,24 @@
 	#define float_number float
 #endif
 
+typedef struct vector vector;
+struct vector
+{
+	float_number xi;
+	float_number yj;
+	float_number zk;
+};
+
+typedef struct quaternion quaternion;
+struct quaternion
+{
+	// scalar component
+	float_number sc;
+
+	// i, j, and k component
+	float_number xi;
+	float_number yj;
+	float_number zk;
+};
+
 #endif
