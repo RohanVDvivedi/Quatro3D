7a8e3f392895354bd9815d8e1d490cefeba90f91
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-06 18:50:52 +0530

    added some tests

--- test/test.c
+++ test/test.c
@@ -35,8 +35,28 @@ void test_quaternion_compose_decompose(float angle, vector v)
 	printf("\n");
 }
 
+void test_vector_components(vector v, vector dir)
+{
+	printf("TEST : vector components\n");
+	vector parallel;
+	vector_parallel_component(&parallel, &v, &dir);
+
+	vector perpendicular;
+	vector_perpendicular_component(&perpendicular, &v, &dir);
+
+	printf("vec : "); print_vector(v); printf("\n");
+	printf("dir : "); print_vector(dir); printf("\n");
+	printf("||| : "); print_vector(parallel); printf("\n");
+	printf("_|_ : "); print_vector(perpendicular); printf("\n");
+
+	printf("\n");
+}
+
 int main()
 {
+	test_vector_components((vector){3.9, 1.9, -1.6}, (vector){2.0, 2.5, -2.6});
+	test_vector_components((vector){-3.0, 2.5, 2.6}, unit_vector_y_axis);
+	test_vector_components((vector){-3.0, 2.5, 2.6}, (vector){9.3, 1.3, -7.3});
 	test_quaternion_compose_decompose(-2.5, (vector){3.0, 1.5, -7.6});
 	return 0;
 }
